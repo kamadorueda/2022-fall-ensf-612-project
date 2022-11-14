044b1831a64263d78b0e2ea95c0615843ee3749b
Kevin Amado <kamadorueda@gmail.com>
2022-11-13 17:07:28 -0700

    step-0: get comments script

---
+++ step-0-raw-data/get-comments.sh
@@ -0,0 +1,18 @@
+#! /bin/sh -eux
+
+cat issue-numbers.lst | while read -r issue_number; do
+  page=0
+  while true; do
+    page=$((page + 1))
+    sleep 2
+    gh api \
+      -H "Accept: application/vnd.github+json" \
+      "/repos/PowerShell/PowerShell/issues/${issue_number}/comments?per_page=100&page=${page}" \
+      > "comments/issue-${issue_number}-page-${page}.json"
+
+    if jq -er 'length == 0' < "comments/issue-${issue_number}-page-${page}.json"; then
+      rm "comments/issue-${issue_number}-page-${page}.json"
+      break
+    fi
+  done
+done
