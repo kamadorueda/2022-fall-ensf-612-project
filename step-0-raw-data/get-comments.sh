e0e878889a5a408270325ff6ffe981dc4bba55cf
Kevin Amado <kamadorueda@gmail.com>
2022-11-15 13:15:57 -0700

    step-0: add comments

--- step-0-raw-data/get-comments.sh
+++ step-0-raw-data/get-comments.sh
@@ -4,7 +4,7 @@ cat issue-numbers.lst | while read -r issue_number; do
   page=0
   while true; do
     page=$((page + 1))
-    sleep 2
+    sleep 1
     gh api \
       -H "Accept: application/vnd.github+json" \
       "/repos/PowerShell/PowerShell/issues/${issue_number}/comments?per_page=100&page=${page}" \
