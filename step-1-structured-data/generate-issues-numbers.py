3606e95c9ed13baea890481d5f2505368410b58e
Kevin Amado <kamadorueda@gmail.com>
2022-11-19 15:56:13 -0700

    step-1: issue numbers

---
+++ step-1-structured-data/generate-issues-numbers.py
@@ -0,0 +1,22 @@
+import glob
+import json
+
+
+def main() -> None:
+    issue_numbers = []
+    for issue_path in glob.glob("step-1-structured-data/issues/*.json"):
+        with open(issue_path, mode="r", encoding="utf-8") as issues_file:
+            issue = json.load(issues_file)
+        issue_numbers.append(issue["number"])
+
+    issue_numbers.sort(reverse=True)
+    with open(
+        f"step-1-structured-data/issue-numbers.json",
+        mode="w",
+        encoding="utf-8",
+    ) as file:
+        json.dump(issue_numbers, file, indent=2, sort_keys=True)
+
+
+if __name__ == "__main__":
+    main()
